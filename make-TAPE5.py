17979f8b27ab7d7d39242e34d2dfdcb1f3ba69a6
satpyscene <2080566723@qq.com>
2024-07-24 17:49:28 +0800

    整理结构，添加注释

--- make-TAPE5.py
+++ make-TAPE5.py
@@ -2,10 +2,19 @@ import numpy as np
 import math
 import subprocess
 import shutil
-
+# 1.下面是大气分层数（廓线文件行数）和廓线数（廓线文件列数）
 lev = 55
 prof = 50
 lev1 = 55
+# 2.下面三行：maxwn为波数区间（一次模拟中期望的范围，LBLRTM要求其最大不能超过2000，因此如果想模拟更大的范围就需要用循环逐个波段模拟后拼接），bwn0为开始波数，ewn0为结束波数。
+maxwn = 1000.0
+bwn0 = 6500.0
+ewn0 = 8500.0
+# 3.分辨率
+resolution = '1.000E-01'
+# 4.修改影响输出的气体种类在后面循环中的这一行修改：file.write(f"{wv[i, iprof]:15.8E}{blank[i]:15.8E}{blank[i]:15.8E}
+
+# 5.修改大气廓线文件大约在五六十行的位置，修改路径，并且注意对应廓线文件分层及廓线数量与最开始设置
 
 p = np.zeros((lev, prof), dtype=np.float32)
 t = np.zeros((lev, prof), dtype=np.float32)
@@ -26,18 +35,14 @@ trah2 = np.zeros((lev1, 4), dtype=np.float32)
 tra = np.zeros((lev, 4), dtype=np.float32)
 trah = np.zeros((lev, 4), dtype=np.float32)
 
-angle = 0.0
 va = np.zeros(6, dtype=np.float32)
 height = np.zeros(lev, dtype=np.float32)
 pp = np.zeros(lev, dtype=np.float32)
 tsurf = np.zeros(prof, dtype=np.float32)
 scant = np.array([2.58, 2.94, 3.72, 4.83, 6.1, 7.2, 9], dtype=np.float32)
 blank = np.zeros(lev, dtype=np.float32)
-bwn = 0.0
-ewn = 0.0
-maxwn = 1000.0
-bwn0 = 6500.0
-ewn0 = 8500.0
+
+
 lay = ['001', '002', '003', '004', '005', '006', '007', '008', '009', '010',
        '011', '012', '013', '014', '015', '016', '017', '018', '019', '020',
        '021', '022', '023', '024', '025', '026', '027', '028', '029', '030',
@@ -48,7 +53,9 @@ lay = ['001', '002', '003', '004', '005', '006', '007', '008', '009', '010',
 # Read pressure data
 with open('./profile/pressure.txt', 'r') as file:
     for i in range(lev):
+        # 将廓线文件的一整行都读给pl（行数即为层数，列数代表廓线数）
         p1[i, :] = np.array(file.readline().split(), dtype=np.float32)
+# 下边是将气压倒序排列
 for i in range(lev):
     p[i, :] = p1[lev - i - 1, :]
 
@@ -96,18 +103,22 @@ for iadd in range(1):
             with open('TAPE5', 'w') as file:
                 CXID = '83P 55level profile to creat transmittance database'
                 file.write(f"${CXID}\n")
+                # 下面一行是LBLRTM设置参数，具体作用查阅html说明文档
                 file.write(f" HI=1 F4=1 CN=1 AE=0 EM=0 SC=0 FI=0 PL=0 TS=0 AM=1 MG=0 LA=0 OD=1 XS=0   00   00\n")
-                file.write(f"{bwn:10.3f}{ewn:10.3f}                                                            REJ=0     1.000E-01\n")
+                # 下面最后一个位置设置分辨率
+                file.write(f"{bwn:10.3f}{ewn:10.3f}                                                            REJ=0     {resolution}\n")
                 file.write(f"{0:5}{2:5}{-55:5}{1:5}{1:5}{7:5}{1:5}{0:5}{0:10.3f}{0:10.3f}\n")
                 file.write(f"{p[lev-1, iprof]:10.3f}{p[0, iprof]:10.3f}{180.000:10.3f}\n")
                 column_data = p[:, iprof]
-                # 以8个一组分批写入数据
+                # 以8个一组分批写入气压数据
                 file.writelines([''.join([f"{x:10.3f}".rstrip() for x in column_data[i:i+8]]) + '\n' for i in range(0, len(column_data), 8)])
                 # file.writelines([f"{'   '.join([f'{x:10.3f}' for x in column_data[i:i+8]])}\n" for i in range(0, len(column_data), 8)])
                 file.write(f"{-55:5}         {'INPUT FOR CAMEX'}\n")
                 for i in range(lev):
                     file.write(f"{height[i]:10.3f}{p[i, iprof]:10.3f}{t[i, iprof]:10.3f}     {'AA'} {'L'} {'AAAAAAA'}\n")
                     file.write(f"{wv[i, iprof]:15.8E}{blank[i]:15.8E}{blank[i]:15.8E}{blank[i]:15.8E}{blank[i]:15.8E}{blank[i]:15.8E}{blank[i]:15.8E}\n")
+                # 用哪个开哪个WRITE(44,"(8E15.8)") wv(i,iprof),tra(i,1),ozo(i,iprof),blank(i),blank(i),blank(i),blank(i)!,blank(i),blank(i),blank(i),blank(i),blank(i),tra(i,10),blank(i),blank(i),tra(i,11)!这里开几个气体跟上面的A保持一致，不用的气体就blank
+                
                 file.write(f"-1.0\n")
                 file.write(f"$ Transfer to ASCII plotting data\n")
                 file.write(f" HI=0 F4=0 CN=0 AE=0 EM=0 SC=0 FI=0 PL=1 TS=0 AM=0 MG=0 LA=0 OD=0 XS=0   0   0\n")
